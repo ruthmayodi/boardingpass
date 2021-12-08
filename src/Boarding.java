894208e8555a68025df20c9c0e36a0505aa6e601
Chris Baxter <baxtercrb9@gmail.com>
2021-12-08 13:05:00 -0500

    Added Scaffolding (Constructors, Getters/Setters, Attributes)

--- src/Boarding.java
+++ src/Boarding.java
@@ -1,2 +1,81 @@
+
+import java.util.*;
+
 public class Boarding {
+
+    private int passNumber;
+    private Date date;
+    private String origin;
+    private String destination;
+    private Date eta;
+    private Date departureTime;
+
+    public Boarding(int passNumber, Date date, String origin, String destination, Date eta, Date departureTime){
+        this.passNumber = passNumber;
+        this.date = date;
+        this.origin = origin;
+        this.destination = destination;
+        this.eta = eta;
+        this.departureTime = departureTime;
+    }
+
+    public int getPassNumber() {
+        return passNumber;
+    }
+
+    public void setPassNumber(int passNumber) {
+        this.passNumber = passNumber;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public Date getEta() {
+        return eta;
+    }
+
+    public void setEta(Date eta) {
+        this.eta = eta;
+    }
+
+    public Date getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Date departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Boarding Pass Number: ").append(this.passNumber).append("\n");
+        sb.append("Date: ").append(this.date).append("\n");
+        sb.append("Origin: ").append(this.origin).append("\n");
+        sb.append("Destination: ").append(this.destination).append("\n");
+        sb.append("ETA: ").append(this.eta).append("\n");
+        sb.append("Departure Time: ").append(this.departureTime).append("\n");
+        return sb.toString();
+    }
 }
--- src/Passenger.java
+++ src/Passenger.java
@@ -1,2 +1,70 @@
+
+import java.util.*;
+
 public class Passenger {
+
+    private String name;
+    private String email;
+    private int phone;
+    private String gender;
+    private int age;
+
+    public Passenger(String name, String email, int phone, String gender, int age){
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Passenger Name: ").append(this.name).append("\n");
+        sb.append("Passenger Email: ").append(this.email).append("\n");
+        sb.append("Passenger Phone Number: ").append(this.phone).append("\n");
+        sb.append("Passenger Gender: ").append(this.gender).append("\n");
+        sb.append("Passenger Age: ").append(this.age).append("\n");
+        return sb.toString();
+    }
 }
